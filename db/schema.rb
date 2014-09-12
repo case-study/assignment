4a34c7b2a73825767f532ec2c111766821c84aae
M. Shafiq <muhammad@dubizzle.com>
2014-09-12 18:15:40 +0400

    devise user setup authentication and attached to collection

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912123748) do
+ActiveRecord::Schema.define(version: 20140912134124) do
 
   create_table "collections", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
